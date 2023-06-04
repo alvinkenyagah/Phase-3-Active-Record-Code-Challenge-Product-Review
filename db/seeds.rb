47c4a3eea71947f6305c464e1ba0771df8c0980d
alvinkenyagah <alvin47k@gmail.com>
2023-06-04 17:34:12 +0300

    Add ERD diagram

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# Create sample users
+5.times do
+    User.create(name: Faker::Name.name)
+  end
+  
+  # Create sample products
+  10.times do
+    Product.create(item: Faker::Commerce.product_name, price: Faker::Commerce.price(range: 10..100))
+  end
+  
+  # Create sample reviews
+  30.times do
+    Review.create(
+      rating: Faker::Number.between(from: 1, to: 5),
+      comment: Faker::Lorem.sentence,
+      product_id: Product.pluck(:id).sample
+    )
+  end
+  
